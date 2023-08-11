71648cdcb26ac4bd7eb022043c9b924867dde6dc
pawel47 <pawelwierzbicki@rocketmail.com>
2023-08-11 17:29:57 +0000

    q4
    Signed-off-by: pawel47 <pawelwierzbicki@rocketmail.com>

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include "stdio.h"
+
+int a = 10;
+int b = 20;
+int c = 30;
+int d = a + b ; 
+int e = d-c;
+int main(void) {
+    printf("value d is %d,\n", d);
+    printf("value e is %d,\n", e);
+}
+    
